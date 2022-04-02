c29cf0a4b80d4999acb248d1774d5abfe6d570ed
大漠穷秋 <damoqiongqiu@126.com>
2022-04-02 21:34:24 +0800

    delete fabric

--- public/typeorm-entity-designer/src/index.js
+++
@@ -1,6 +0,0 @@
-import './component/entity.class';
-import './component/relation.class';
-import './mixins/canvas.mixin';
-import './mixins/math_vector.mixin';
-import './mixins/object.mixin';
-import './mixins/object_geometry_plus.mixin';
